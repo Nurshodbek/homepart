88081aa3454669b78cdab78bda7c2db16e7f469a
Nurshodbek <82872164+Nurshodbek@users.noreply.github.com>
2021-04-23 02:46:09 +0500

    Add files via upload

---
+++ all code/jk/app.js
@@ -0,0 +1,50 @@
+// let age = prompt ("what is your age?");
+
+// let welcome = (age < 18) ?
+// () => alert('hello') :
+// () => alert("Greetings"); 
+
+// welcome();
+
+// for(let i = 0; i <=5; i++) {
+//     if (i%2 !==0)
+//     console.log(i);
+// }
+
+// let i = 0;
+// while (i <= 10) {
+//     if (i%2 !==0) {
+//        i++;
+//        continue;
+//     }
+//     console.log(i);
+//     i++;
+//}
+// console.log(document);
+// const p = document.getElementById("p1");
+// const button = document.querySelector("#click");
+// const input = document.querySelector(".myinput");
+
+// console.log(input.value);
+// console.log(p.textContent);
+// console.log(p);
+// console.log(button);
+// console.log(input);
+
+// let month = 28 ; 
+// if (month <=30 || month < 31 ) {
+//     console.log('December', 31);
+// }
+// else if (month <=28 || month < 29 ) {
+//     console.log('february', 28);
+// }
+// else if (month <=29 || month < 30  ) {
+//     console.log('january', 30);
+// }
+// else {
+//     console.log('March', 31);
+
+// }
+
+
+
